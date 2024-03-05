04110c5b4069195ba6a774ca6c64bdeef11d78ca
Geila Martins <geila.estudos@gmail.com>
2024-03-05 10:06:18 -0300

    Realizando alterações no admin do Django e integrando com tabelas criado em models.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto, Cliente # importando do models para visualizar na interface do admin
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
+
+admin.site.register(Produto, ProdutoAdmin)
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+    
+admin.site.register(Cliente, ClienteAdmin)
--- core/models.py
+++ core/models.py
@@ -5,7 +5,15 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8) # duas casas decimais e o número máximo de dígitos
     estoque = models.IntegerField('Quantidade em Estoque')
 
+    def __str__(self):
+        return self.nome
+
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
+
+
