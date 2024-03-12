d898f7e9e41798f5f8cc57110e17883ab668ea06
Geila Martins <geila.estudos@gmail.com>
2024-03-12 20:24:25 -0300

    Verificando o usuário logado

--- core/views.py
+++ core/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 def index(request):
+    print(dir(request.user))
+    print(f'User: {request.user}')
+
+    # verifica se o usuário está logado
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuário não logado!'
+    else:
+        teste = 'Usuário logado!'
+    # finaliza a verificação do usuário logado
+        
     context = {
     'curso':'Curso de Python com Framework Django',
-    'outro':'Agora eu aprendo Django'
+    'outro':'Agora eu aprendo Django',
+    'logado': teste
     }
     return render(request, 'index.html', context)
 
